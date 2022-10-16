4c5dff270ba57bed33f7e6c8d839c0e993736f34
VectorBatesta <48493157+VectorBatesta@users.noreply.github.com>
2022-10-16 13:09:04 -0300

    2

--- trabalho.m
+++ trabalho.m
@@ -1,29 +1,45 @@
 %% Projeto de Sistemas Lineares
 % 16/09/2022
+% aluno: Vitor Batista
+% prof: Daniel Campos
 
 close all
 clc
 clear
 
 %% Gravar áudios
-Tempo = 10; %5 segundos de gravação
-FS = 44000; %Frequência de amostragem em 44 kHz
-Nbits = 16; %sinal de 16 bits
+Tempo = 5; %5 segundos de gravação
+FS = 8000; %Frequência de amostragem em 8 kHz
+Nbits = 8; %sinal de 8 bits
+%8 bits x 8000/s = 8000 bytes/s ou 8kBps
 Canais = 1; %1 = mono, 2 = stereo
 
 vozGravacao = audiorecorder(FS, Nbits, Canais); %preparar para gravar
 ruidoGravacao = audiorecorder(FS, Nbits, Canais);
 
+disp("Voz gravando.");
 record(vozGravacao);
 pause(Tempo);
 stop(vozGravacao);
 
+disp("Ruido gravando.");
 record(ruidoGravacao);
 pause(Tempo);
 stop(ruidoGravacao);
 
+disp("Término.");
+
 %guardar voz em uma variavel double
 vozSinal = getaudiodata(vozGravacao, 'double');
-%guardar ruidoem uma variavel double
+subplot(3,1,1);
+plot(vozSinal);
+
+%guardar ruido em uma variavel double
 ruidoSinal = getaudiodata(ruidoGravacao, 'double');
+subplot(3,1,2);
+plot(ruidoSinal);
+
+somaSinal = vozSinal + ruidoSinal;
+subplot(3,1,3);
+plot(somaSinal);
 

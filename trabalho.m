454482933b6d2379e89567fb1a4b8802e4973ef7
VectorBatesta <48493157+VectorBatesta@users.noreply.github.com>
2022-10-17 19:08:39 -0300

    3

--- trabalho.m
+++ trabalho.m
@@ -9,37 +9,48 @@ clear
 
 %% Gravar áudios
 Tempo = 5; %5 segundos de gravação
-FS = 8000; %Frequência de amostragem em 8 kHz
-Nbits = 8; %sinal de 8 bits
-%8 bits x 8000/s = 8000 bytes/s ou 8kBps
+FS = 44100; %Frequência de amostragem em 44.1 kHz
+Nbits = 16; %sinal de 16 bits
+%16 bits x 44100 Hz = 705600 bps ou 705.6 Kbps
 Canais = 1; %1 = mono, 2 = stereo
 
-vozGravacao = audiorecorder(FS, Nbits, Canais); %preparar para gravar
-ruidoGravacao = audiorecorder(FS, Nbits, Canais);
+vozAudio = audiorecorder(FS, Nbits, Canais); %preparar para gravar
+ruidoAudio = audiorecorder(FS, Nbits, Canais);
 
+%% gravar voz
 disp("Voz gravando.");
-record(vozGravacao);
+record(vozAudio);
 pause(Tempo);
-stop(vozGravacao);
+stop(vozAudio);
 
+disp("Término gravacao voz.");
+
+%% gravar ruido
 disp("Ruido gravando.");
-record(ruidoGravacao);
+record(ruidoAudio);
 pause(Tempo);
-stop(ruidoGravacao);
+stop(ruidoAudio);
 
-disp("Término.");
+disp("Término gravacao ruido.");
 
+%% traduzir sinais gravados para vetor, somar ruido e voz + plotar 
 %guardar voz em uma variavel double
-vozSinal = getaudiodata(vozGravacao, 'double');
+vozSinal = getaudiodata(vozAudio, 'double');
 subplot(3,1,1);
 plot(vozSinal);
 
 %guardar ruido em uma variavel double
-ruidoSinal = getaudiodata(ruidoGravacao, 'double');
+ruidoSinal = getaudiodata(ruidoAudio, 'double');
 subplot(3,1,2);
 plot(ruidoSinal);
 
 somaSinal = vozSinal + ruidoSinal;
 subplot(3,1,3);
 plot(somaSinal);
+
+%% tocar audio
+somaAudio = audioplayer(somaSinal,FS);
+play(somaAudio);
+disp("tocando audio resultante");
+
 

cac92ba1e560c48d04df3129f2e02b7ee50c9797
VectorBatesta <48493157+VectorBatesta@users.noreply.github.com>
2022-11-27 19:57:10 -0300

    Menu criado e configurado
    achei um menu() na net e atribui funçoes pra cada um dos bagulhos pra funcionar com o menu, talvez as funções nao estao retornando valores corretos, precisa ser testado

--- src/trabalho.m
+++ src/trabalho.m
@@ -22,312 +22,375 @@ Canais = 1; %1 = mono, 2 = stereo
 vozAudio = audiorecorder(FS, Nbits, Canais); %preparar para gravar
 ruidoAudio = audiorecorder(FS, Nbits, Canais);
 
-%% gravar voz
-disp("Voz gravando.");
-record(vozAudio);
-pause(Tempo);
-stop(vozAudio);
+%% menu
+selecao = menu("Selecione uma opção:", ...
+    "Gravar voz", "Gravar ruído", "Carregar áudio já gravado", ...
+    "Plotar Sinais", "Manipular Sinais", "Tocar áudios", "Calcular SNR", ...
+    "Realizar Média Móvel usando Convolução", ...
+    "Definir melhor intervalo de Filtragem", "Plotar Resultados", ...
+    "Realizar Espectro de Frequência e plotar", "TUDO", "Sair");
 
-disp("Término gravacao voz.");
+switch(selecao)
+    case 1
+        gravarVoz();
+    case 2
+        gravarRuido();
+    case 3
+        loadAudio();
+    case 4
+        plotSinais();
+    case 5
+        manipulacaoSinais();
+    case 6
+        tocarSinal();
+    case 7
+        calcularSNR();
+    case 8
+        mediaMovelConvolucao();
+    case 9
+        melhorIntervaloFiltragem();
+    case 10
+        plotResultados();
+    case 11
+        plotEspectro();
+    case 12
+        gravarVoz();
+        gravarRuido();
+        loadAudio();
+        plotSinais();
+        manipulacaoSinais();
+        tocarSinal();
+        calcularSNR();
+        mediaMovelConvolucao();
+        melhorIntervaloFiltragem();
+        plotResultados();
+        plotEspectro();
+end
+
+%% gravar voz
+function gravarVoz()
+    disp("Voz gravando.");
+    record(vozAudio);
+    pause(Tempo);
+    stop(vozAudio);
+    
+    disp("Término gravacao voz.");
+end
 
 %% workaround erro de tam diferente
-pause(1);
+%pause(1);
 
 %% gravar ruido
-disp("Ruido gravando.");
-record(ruidoAudio);
-pause(Tempo);
-stop(ruidoAudio);
-
-disp("Término gravacao ruido.");
+function gravarRuido()
+    disp("Ruido gravando.");
+    record(ruidoAudio);
+    pause(Tempo);
+    stop(ruidoAudio);
+    
+    disp("Término gravacao ruido.");
+end
 
 %% load audio
-% load("Projeto_AnaliseSistemas-main\src\variaveisDeTeste.mat")
+function loadAudio()
+    load("Projeto_AnaliseSistemas-main\src\variaveisDeTeste.mat")
+end
 
 %% traduzir sinais gravados para vetor, somar ruido e voz + plotar 
-% janela
-figure('Name','Gráficos dos áudios','NumberTitle','off');
-
-% guardar voz em uma variavel double
-vozSinal = getaudiodata(vozAudio, 'double'); %transforma pra vetor double
-
-% obtem o tamanho do sinal
-tamSinal = length(vozSinal);
-
-% delimitador em x (converte a escala de amostras para segundos)
-DelimitadorEmX = (0:tamSinal-1)/FS;
-
-% plota
-subplot(3,1,1);
-plot(DelimitadorEmX, vozSinal,'r');
-ylim([-1 1]);
-grid("on");
-title("Sinal da gravação da voz");
-xlabel("Tempo em segundos");
-ylabel("Amplitude");
-%xticks([0:0.25:5]);
-%yticks([-1:0.25:1]);
-
-% guardar ruido em uma variavel double
-ruidoSinal = getaudiodata(ruidoAudio, 'double');
-subplot(3,1,2);
-plot(DelimitadorEmX, ruidoSinal,'k');
-ylim([-1 1]);
-grid("on");
-title("Sinal da gravação do ruído");
-xlabel("Tempo em segundos");
-ylabel("Amplitude");
-
-somaSinal = vozSinal + ruidoSinal;
-subplot(3,1,3);
-plot(DelimitadorEmX, somaSinal,'b');
-ylim([-1 1]);
-grid("on");
-title("Sinal da soma entre os dois áudios");
-xlabel("Tempo em segundos");
-ylabel("Amplitude");
+function plotSinais()
+    % janela
+    figure('Name','Gráficos dos áudios','NumberTitle','off');
+    
+    % guardar voz em uma variavel double
+    vozSinal = getaudiodata(vozAudio, 'double'); %transforma pra vetor double
+    
+    % obtem o tamanho do sinal
+    tamSinal = length(vozSinal);
+    
+    % delimitador em x (converte a escala de amostras para segundos)
+    DelimitadorEmX = (0:tamSinal-1)/FS;
+    
+    % plota
+    subplot(3,1,1);
+    plot(DelimitadorEmX, vozSinal,'r');
+    ylim([-1 1]);
+    grid("on");
+    title("Sinal da gravação da voz");
+    xlabel("Tempo em segundos");
+    ylabel("Amplitude");
+    %xticks([0:0.25:5]);
+    %yticks([-1:0.25:1]);
+    
+    % guardar ruido em uma variavel double
+    ruidoSinal = getaudiodata(ruidoAudio, 'double');
+    subplot(3,1,2);
+    plot(DelimitadorEmX, ruidoSinal,'k');
+    ylim([-1 1]);
+    grid("on");
+    title("Sinal da gravação do ruído");
+    xlabel("Tempo em segundos");
+    ylabel("Amplitude");
+    
+    somaSinal = vozSinal + ruidoSinal;
+    subplot(3,1,3);
+    plot(DelimitadorEmX, somaSinal,'b');
+    ylim([-1 1]);
+    grid("on");
+    title("Sinal da soma entre os dois áudios");
+    xlabel("Tempo em segundos");
+    ylabel("Amplitude");
+end
 
 %% manipulaçao
 
-% 0 = nada
-% 1 = modular por funçao
-% 2 = clipping
-% 3 = inverter
-% 4 = echo
-manipulacao = 3;
-limiteClipping = 0.5;
-funcao = linspace(1,6,tamSinal);
-
-
-if (manipulacao ~= 0)
-    sinalManipulado = zeros(1, tamSinal + 1);
-end
-
-% multiplicação do sinal por uma função
-if (manipulacao == 1)
-    sinalManipulado = zeros(1, tamSinal);
-    for i = 1:tamSinal
-        funcao(i) = exp(-funcao(i));
-        sinalManipulado(i) = somaSinal(i)*funcao(i);
+function manipulacaoSinais()
+    % 0 = nada
+    % 1 = modular por funçao
+    % 2 = clipping
+    % 3 = inverter
+    % 4 = echo
+    manipulacao = 3;
+    limiteClipping = 0.5;
+    funcao = linspace(1,6,tamSinal);
+    
+    
+    if (manipulacao ~= 0)
+        sinalManipulado = zeros(1, tamSinal + 1);
     end
-end
-
-% manipulação da amplitude
-if (manipulacao == 2)
-    sinalManipulado = zeros(1, tamSinal);
-    for i = 1: tamSinal
-        if(somaSinal(i) > limiteClipping)
-            sinalManipulado(i) = limiteClipping;
-        elseif(somaSinal(i) < -limiteClipping)
-            sinalManipulado(i) = -limiteClipping;
-        else
+    
+    % multiplicação do sinal por uma função
+    if (manipulacao == 1)
+        sinalManipulado = zeros(1, tamSinal);
+        for i = 1:tamSinal
+            funcao(i) = exp(-funcao(i));
+            sinalManipulado(i) = somaSinal(i)*funcao(i);
+        end
+    end
+    
+    % manipulação da amplitude
+    if (manipulacao == 2)
+        sinalManipulado = zeros(1, tamSinal);
+        for i = 1: tamSinal
+            if(somaSinal(i) > limiteClipping)
+                sinalManipulado(i) = limiteClipping;
+            elseif(somaSinal(i) < -limiteClipping)
+                sinalManipulado(i) = -limiteClipping;
+            else
+                sinalManipulado(i) = somaSinal(i);
+            end    
+        end
+    
+    end
+    
+    if (manipulacao == 3)
+        sinalManipulado = zeros(1, tamSinal);
+        for i = 1: tamSinal
+            sinalManipulado(tamSinal-i) = somaSinal(i);
+        end
+    end
+    
+    % ECHO
+    if (manipulacao == 4)
+        sinalManipulado = zeros(1, tamSinal);
+        
+        for i = 1 : 10000
             sinalManipulado(i) = somaSinal(i);
-        end    
-    end
-
-end
-
-if (manipulacao == 3)
-    sinalManipulado = zeros(1, tamSinal);
-    for i = 1: tamSinal
-        sinalManipulado(tamSinal-i) = somaSinal(i);
-    end
-end
-
-% ECHO
-if (manipulacao == 4)
-    sinalManipulado = zeros(1, tamSinal);
-    
-    for i = 1 : 10000
-        sinalManipulado(i) = somaSinal(i);
+        end
+        
+        for i = 10001: tamSinal
+            sinalManipulado(i) = somaSinal(i) + (somaSinal(i-10000))/3;
+        end
+        
+        for i = tamSinal + 1 : tamSinal + tamSinal/5
+            sinalManipulado(i) = (sinalManipulado(i-10000))/3;
+        end
+        
     end
     
-    for i = 10001: tamSinal
-        sinalManipulado(i) = somaSinal(i) + (somaSinal(i-10000))/3;
-    end
+    if (manipulacao ~= 0)
+        tocar = audioplayer(sinalManipulado,FS);
+        DelimitadorEmXManipulado = (0:length(sinalManipulado)-1)/FS;
+        plot(DelimitadorEmXManipulado,sinalManipulado,'g');
+        ylim([-1 1])
+        grid("on");
+        title("Sinal da soma entre os dois áudios manipulado");
+        xlabel("Tempo em segundos");
+        ylabel("Amplitude");
+        disp("Tocando sinal manipulado");
+        play(tocar);
     
-    for i = tamSinal + 1 : tamSinal + tamSinal/5
-        sinalManipulado(i) = (sinalManipulado(i-10000))/3;
+        pause(Tempo);
     end
-    
-end
-
-if (manipulacao ~= 0)
-    tocar = audioplayer(sinalManipulado,FS);
-    DelimitadorEmXManipulado = (0:length(sinalManipulado)-1)/FS;
-    plot(DelimitadorEmXManipulado,sinalManipulado,'g');
-    ylim([-1 1])
-    grid("on");
-    title("Sinal da soma entre os dois áudios manipulado");
-    xlabel("Tempo em segundos");
-    ylabel("Amplitude");
-    disp("Tocando sinal manipulado");
-    play(tocar);
-
-    pause(Tempo);
 end
 
 %% tocar voz, ruido e voz resultante
-clc;
-
-% 0: tocar voz
-% 1: tocar ruido
-% 2: tocar soma
-% 3: tocar todos
-selecaoTocar = 3;
-
-% tocar voz
-if (selecaoTocar == 0 || selecaoTocar == 3)
-    tocar = audioplayer(vozSinal,FS);
-    play(tocar);
-    disp("Tocando voz");
-
-    pause(Tempo);
-end
-
-% tocar ruido
-if (selecaoTocar == 1 || selecaoTocar == 3)
-    tocar = audioplayer(ruidoSinal,FS);
-    play(tocar);
-    disp("Tocando ruido");
+function tocarSinal()
+    clc;
     
-    pause(Tempo);
-end
-
-% tocar soma
-if (selecaoTocar == 2 || selecaoTocar == 3)
-    tocar = audioplayer(somaSinal,FS);
-    play(tocar);
-    disp("Tocando audio resultante");
+    % 0: tocar voz
+    % 1: tocar ruido
+    % 2: tocar soma
+    % 3: tocar todos
+    selecaoTocar = 3;
     
-    pause(Tempo);
+    % tocar voz
+    if (selecaoTocar == 0 || selecaoTocar == 3)
+        tocar = audioplayer(vozSinal,FS);
+        play(tocar);
+        disp("Tocando voz");
+    
+        pause(Tempo);
+    end
+    
+    % tocar ruido
+    if (selecaoTocar == 1 || selecaoTocar == 3)
+        tocar = audioplayer(ruidoSinal,FS);
+        play(tocar);
+        disp("Tocando ruido");
+        
+        pause(Tempo);
+    end
+    
+    % tocar soma
+    if (selecaoTocar == 2 || selecaoTocar == 3)
+        tocar = audioplayer(somaSinal,FS);
+        play(tocar);
+        disp("Tocando audio resultante");
+        
+        pause(Tempo);
+    end
 end
 
 %% SNR
-EnergiaVoz = sum(vozSinal(:).^2); %Energia do sinal da Voz
-EnergiaRuido = sum(ruidoSinal(:).^2); %Energia do Ruído
-
-PotenciaVoz = EnergiaVoz / length(vozSinal); %Potência do sinal da Voz
-PotenciaRuido = EnergiaRuido / length(ruidoSinal); %Potência do Ruído
-
-SNR = 10 * log10(PotenciaVoz / PotenciaRuido);
-disp("SNR = " + SNR + " dB");
+function calcularSNR()
+    EnergiaVoz = sum(vozSinal(:).^2); %Energia do sinal da Voz
+    EnergiaRuido = sum(ruidoSinal(:).^2); %Energia do Ruído
+    
+    PotenciaVoz = EnergiaVoz / length(vozSinal); %Potência do sinal da Voz
+    PotenciaRuido = EnergiaRuido / length(ruidoSinal); %Potência do Ruído
+    
+    SNR = 10 * log10(PotenciaVoz / PotenciaRuido);
+    disp("SNR = " + SNR + " dB");
+end
 
 %% media movel em convolução otimizada
-
-rangeDaMedia = 1:1:200;
-E = zeros(length(rangeDaMedia),1);
-% "zeros()" cria vetor/matriz de zeros 
-k = 1;
-for a = rangeDaMedia
-    size = a+a+1;
-    respostaImpulso = ones(size, 1)/size;
-    resultadoFiltro = conv(somaSinal,respostaImpulso,'same');
-    residuo = vozSinal - resultadoFiltro;
-    E(k) = sum(residuo.^2);
-    k = k+1;
+function mediaMovelConvolucao()
+    rangeDaMedia = 1:1:200;
+    E = zeros(length(rangeDaMedia),1);
+    % "zeros()" cria vetor/matriz de zeros 
+    k = 1;
+    for a = rangeDaMedia
+        size = a+a+1;
+        respostaImpulso = ones(size, 1)/size;
+        resultadoFiltro = conv(somaSinal,respostaImpulso,'same');
+        residuo = vozSinal - resultadoFiltro;
+        E(k) = sum(residuo.^2);
+        k = k+1;
+    end
 end
 
 %% Determinando Melhor Intervalo de Filtragem
-
-melhorRangeMedia = rangeDaMedia(E == min(E));
-melhorRespostaImpulso = ones(melhorRangeMedia+melhorRangeMedia+1,1)/(melhorRangeMedia+melhorRangeMedia+1);
-melhorResultadoFiltro = conv(somaSinal,melhorRespostaImpulso,'same');
-
+function melhorResultadoFiltro = melhorIntervaloFiltragem()
+    melhorRangeMedia = rangeDaMedia(E == min(E));
+    melhorRespostaImpulso = ones(melhorRangeMedia+melhorRangeMedia+1,1)/(melhorRangeMedia+melhorRangeMedia+1);
+    melhorResultadoFiltro = conv(somaSinal,melhorRespostaImpulso,'same');
+end
 
 %% Plotando Resultados
-
-figure
-%Plotando Energia Residual para cada intervalo de média móvel calculado
-subplot(2,1,1)
-plot(rangeDaMedia,E,'k-o');
-title('Energia Residual por Range de Média Móvel');
-xlabel('Range');
-ylabel('Energia Residual');
-
-% Plotando a forma de onda do melhor resultado do filtro em comparação com
-% osinal original
-subplot(2,1,2)
-hold on
-plot(DelimitadorEmX,vozSinal,'r');
-plot(DelimitadorEmX,melhorResultadoFiltro,'k');
-hold off
-title('Melhor Resultado Filtrado vs Áudio Original');
-legend({'Áudio Original','Melhor Áudio Filtrado'});
-xlabel('Tempo');
-
-tocar = audioplayer(melhorResultadoFiltro,FS,8);
-play(tocar);
-disp("Tocando audio resultante");
+function plotResultados()
+    figure
+    %Plotando Energia Residual para cada intervalo de média móvel calculado
+    subplot(2,1,1)
+    plot(rangeDaMedia,E,'k-o');
+    title('Energia Residual por Range de Média Móvel');
+    xlabel('Range');
+    ylabel('Energia Residual');
+    
+    % Plotando a forma de onda do melhor resultado do filtro em comparação com
+    % osinal original
+    subplot(2,1,2)
+    hold on
+    plot(DelimitadorEmX,vozSinal,'r');
+    plot(DelimitadorEmX,melhorResultadoFiltro,'k');
+    hold off
+    title('Melhor Resultado Filtrado vs Áudio Original');
+    legend({'Áudio Original','Melhor Áudio Filtrado'});
+    xlabel('Tempo');
+    
+    tocar = audioplayer(melhorResultadoFiltro,FS,8);
+    play(tocar);
+    disp("Tocando audio resultante");
+end
 
 %% espectro freq
-% janela
-figure('Name','Espectro dos sinais','NumberTitle','off');
-
-freqVozSinal = fft(vozSinal);
-freqRuido = fft(ruidoSinal);
-freqSomaSinal = fft(somaSinal);
-freqMelhorResultado = fft(melhorResultadoFiltro);
-
-% 0: plotar espectro voz
-% 1: plotar espectro ruido
-% 2: plotar espectro soma
-% 3: plotar espectro audio filtrado
-%   4: plotar espectro todos
-selecaoPlotar = 4;
-
-% fft de voz
-if (selecaoPlotar == 0 || selecaoPlotar == 4)
-    if (selecaoPlotar == 4)
-        subplot(4,1,1)
+function plotEspectro()
+    % janela
+    figure('Name','Espectro dos sinais','NumberTitle','off');
+    
+    freqVozSinal = fft(vozSinal);
+    freqRuido = fft(ruidoSinal);
+    freqSomaSinal = fft(somaSinal);
+    freqMelhorResultado = fft(melhorResultadoFiltro);
+    
+    % 0: plotar espectro voz
+    % 1: plotar espectro ruido
+    % 2: plotar espectro soma
+    % 3: plotar espectro audio filtrado
+    %   4: plotar espectro todos
+    selecaoPlotar = 4;
+    
+    % fft de voz
+    if (selecaoPlotar == 0 || selecaoPlotar == 4)
+        if (selecaoPlotar == 4)
+            subplot(4,1,1)
+        end
+        semilogx(abs(freqVozSinal),'k');
+        title("Espectro de Frequência do Sinal Original")
+        xlabel("Frequência em Hertz");
+        ylabel("Amplitude");
+        xlim([0 inf]);
+        ylim([0 1000]);
+        grid on;
     end
-    semilogx(abs(freqVozSinal),'k');
-    title("Espectro de Frequência do Sinal Original")
-    xlabel("Frequência em Hertz");
-    ylabel("Amplitude");
-    xlim([0 inf]);
-    ylim([0 1000]);
-    grid on;
-end
-
-% fft do ruido
-if (selecaoPlotar == 1 || selecaoPlotar == 4)
-    if (selecaoPlotar == 4)
-        subplot(4,1,2)
+    
+    % fft do ruido
+    if (selecaoPlotar == 1 || selecaoPlotar == 4)
+        if (selecaoPlotar == 4)
+            subplot(4,1,2)
+        end
+        semilogx(abs(freqRuido),'b');
+        title('Espectro de Frequência do Ruído')
+        xlabel("Frequência em Hertz");
+        ylabel("Amplitude");
+        xlim([0 inf]);
+        ylim([0 1000]);
+        grid on;
     end
-    semilogx(abs(freqRuido),'b');
-    title('Espectro de Frequência do Ruído')
-    xlabel("Frequência em Hertz");
-    ylabel("Amplitude");
-    xlim([0 inf]);
-    ylim([0 1000]);
-    grid on;
-end
-
-% fft da soma
-if (selecaoPlotar == 2 || selecaoPlotar == 4)
-    if (selecaoPlotar == 4)
-        subplot(4,1,3)
+    
+    % fft da soma
+    if (selecaoPlotar == 2 || selecaoPlotar == 4)
+        if (selecaoPlotar == 4)
+            subplot(4,1,3)
+        end
+        semilogx(abs(freqSomaSinal));
+        title('Espectro de Frequência do Sinal Somado')
+        xlabel("Frequência em Hertz");
+        ylabel("Amplitude");
+        xlim([0 inf]);
+        ylim([0 1000]);
+        grid on;
     end
-    semilogx(abs(freqSomaSinal));
-    title('Espectro de Frequência do Sinal Somado')
-    xlabel("Frequência em Hertz");
-    ylabel("Amplitude");
-    xlim([0 inf]);
-    ylim([0 1000]);
-    grid on;
-end
-
-% fft do filtrado
-if (selecaoPlotar == 3 || selecaoPlotar == 4)
-    if (selecaoPlotar == 4)
-        subplot(4,1,4)
+    
+    % fft do filtrado
+    if (selecaoPlotar == 3 || selecaoPlotar == 4)
+        if (selecaoPlotar == 4)
+            subplot(4,1,4)
+        end
+        semilogx(abs(freqMelhorResultado),'r');
+        title('Espectro de Frequência do Melhor Sinal Filtrado')
+        xlabel("Frequência em Hertz");
+        ylabel("Amplitude");
+        xlim([0 inf]);
+        ylim([0 1000]);
+        grid on;
     end
-    semilogx(abs(freqMelhorResultado),'r');
-    title('Espectro de Frequência do Melhor Sinal Filtrado')
-    xlabel("Frequência em Hertz");
-    ylabel("Amplitude");
-    xlim([0 inf]);
-    ylim([0 1000]);
-    grid on;
 end
